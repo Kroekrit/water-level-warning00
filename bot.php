9dac5436aaf2e0bce66c9d52062d5ed9b2250ee5
Kroekrit <32546705+Kroekrit@users.noreply.github.com>
2017-10-06 15:33:28 +0700

    f
    f

--- bot.php
+++ bot.php
@@ -17,10 +17,21 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'Hello'
-			];
+			if (strpos($text , 'hello') !== false) {		
+				$messages = [
+					'type' => 'text',
+					'text' => 'Hello'
+				];
+			}else{
+				$messages = [
+					'type' => 'text',
+					'text' => '?????'
+				];
+			}
+
+
+
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
