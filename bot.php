d95745e1d6f3b17e50519f0540cbe9ea067e8c9f
Kroekrit <32546705+Kroekrit@users.noreply.github.com>
2017-10-12 16:17:55 +0700

    dfs
    fd

--- bot.php
+++ bot.php
@@ -40,13 +40,13 @@ if (!is_null($events['events'])) {
 			}else if (strpos($text , 'Rain') !== false) {	
 				$messages = [
 					'type' => 'text',
-					'text' => $text
+					'text' => $text];
 				$URL_rain = 'https://api.thingspeak.com/channels/345073/feeds/last.json';
 				$json_rain = file_get_contents($URL_rain);
 				$content = json_decode($json_rain);
 				$Last_rain = $content[0]->field1;
 				
-				];
+				
 			}else{
 				$messages = [
 					'type' => 'text',
