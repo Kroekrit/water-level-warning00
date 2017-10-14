96448c50dd62201c111168cd6c17ee27dfb88af7
Kroekrit <32546705+Kroekrit@users.noreply.github.com>
2017-10-14 20:52:32 +0700

    d
    d

--- bot.php
+++ bot.php
@@ -44,7 +44,7 @@ if (!is_null($events['events'])) {
 				$content = json_decode($json_rain);
 				$Last_rain = $content->field1;
 				$Date = $content->created_at;
-				$Time = substr($Date, 11, 8);
+				$Time = date(substr($Date, 11, 8));
 				
 				
 				$messages = [
